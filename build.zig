f8d8b2b64835d92b0219d12f195ec1bbdc1459b4
... <felixutong@gmail.com>
2026-06-09 09:43:20 +0200

    add build function to include target and optimization options for VMA module

--- build.zig
+++ build.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("vma", .{
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const mod = b.addModule("vma", .{
         .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
     });
+    mod.addCSourceFile(.{ .file = b.path("src/vma.cpp"), .flags = &.{} });
+    mod.linkSystemLibrary("c++", .{});
+    mod.linkSystemLibrary("vulkan", .{});
 }
